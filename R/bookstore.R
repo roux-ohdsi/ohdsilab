b747a2675ca259cbd997c9d71e4e9da51db69b38
Rob Cavanaugh <46727239+rbcavanaugh@users.noreply.github.com>
2023-05-09 12:58:39 -0400

    added example to bookstore

--- R/bookstore.R
+++ R/bookstore.R
@@ -2,12 +2,16 @@
 #' Function to Install and Load R Packages
 #'
 #' @param package_names a character vector of package names from CRAN or github
-#' @param description the bookstore() function will check to see if packages provided are
+#' @param description The bookstore() function will check to see if packages provided are
 #' already installed on the local machine. If not installed, it will look for the package on CRAN
 #' and install it if found. If it doesn't find the package on CRAN, it'll ask for the owner/repository_name
 #' from github to install the package from github. this input should be provided without quotes. For example,
 #' respond with roux-ohdsi/ohdsilab NOT "roux-ohdsi/ohdsilab".
 #' @export
+#' @examples
+#' \dontrun{
+#' bookstore(c("aouFI", "CohortGenerator", "tidyr"))
+#' }
 bookstore <- function(package_names){
 
     remaining_packages <- package_names[!(package_names %in% installed.packages()[,"Package"])]

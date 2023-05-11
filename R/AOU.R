24a861cffb1a68b1896d3129bb4f6135bfb81a90
Louisa Smith <louisahsmith@gmail.com>
2023-05-11 19:47:51 -0400

    change some aou function names/params

--- R/AOU.R
+++ R/AOU.R
@@ -3,16 +3,16 @@
 #' Connect to big query database in All of Us
 #'
 #' @param bucket_name variable name for your bucket. Recommend leaving the default.
-#' @param description To use, simply run the function without any arguments. It will
+#' @description To use, simply run the function without any arguments. It will
 #' print a message if you connect successfully. It will also assign your bucket
 #' to an object in your R environment.
 #' @export
 aou_connect <- function(bucket_name = "bucket"){
-  dataset <- str_split_fixed(Sys.getenv('WORKSPACE_CDR'),'\\.', n = 2)
+  dataset <- stringr::str_split_fixed(Sys.getenv('WORKSPACE_CDR'),'\\.', n = 2)
   release <- dataset[2]
   prefix <- dataset[1]
 
-  connection <- dbConnect(
+  connection <- DBI::dbConnect(
     bigrquery::bigquery(),
     billing = Sys.getenv('GOOGLE_PROJECT'),
     project = prefix,
@@ -27,36 +27,36 @@ aou_connect <- function(bucket_name = "bucket"){
            "` to retrieve the name of your bucket"), sep = "\n")
 }
 
-#' Retrieve a file from a bucket to your workspace
+#' Move files from a bucket to your workspace
 #'
-#' @param file_name The name of a file in your bucket or a vector of multiple files.
+#' @param files The name of a file in your bucket or a vector of multiple files.
 #' @param bucket_name Name of your bucket. Recommend leaving the default
-#' @param description This step retrieves a file you have saved permanently in your bucket
-#' into your  where you can read it into R using a function like write.csv().
+#' @description This step retrieves a file you have saved permanently in your bucket
+#' into your workspace where you can read it into R using a function like write.csv().
 #' @export
-aou_retrieve_from_bucket <- function(file_name, bucket_name = "bucket"){
+aou_bucket_to_workspace <- function(files, bucket_name = Sys.getenv('WORKSPACE_BUCKET')){
   # # Copy the file from current workspace to the bucket
-  for (i in 1:length(file_name)){
-    system(paste0("gsutil cp ", get(bucket_name), "/data/", file_name[i], " ."), intern=T)
-    cat("Retrieved ", file_name[i], "\n")
+  for (i in 1:length(files)) {
+    system(paste0("gsutil cp ", bucket_name, "/data/", files[i], " ."), intern = TRUE)
+    cat("Retrieved ", files[i], "\n")
   }
 
 }
 
 #' Save a file from your workspace to your bucket.
 #'
-#' @param file_name name of file to save
+#' @param files name of file to save
 #' @param bucket_name name of your bucket. Recommend leaving the default
-#' @param description This step permanently saves a file you have saved in your workspace
+#' @description This step permanently saves a file you have saved in your workspace
 #' to your bucket where you can always retrieve it. To use, first you need to save the desired
 #' r object as a file (e.g., write.csv(object, filename.csv)) and then run this function
-#' (e.g., aou_save_to_bucket(file_name = "filename.csv")).
+#' (e.g., aou_save_to_bucket(files = "filename.csv")).
 #' @export
-aou_save_to_bucket <- function(file_name, bucket_name = "bucket"){
+aou_workspace_to_bucket <- function(files, bucket_name = Sys.getenv('WORKSPACE_BUCKET')){
   # Copy the file from current workspace to the bucket
-  for(i in 1:length(file_name)){
-    system(paste0("gsutil cp ./", filename, " ", get(bucket_name), "/data/"), intern=T)
-    cat("Saved ", file_name[i], "\n")
+  for(i in 1:length(files)){
+    system(paste0("gsutil cp ./", files[i], " ", bucket_name, "/data/"), intern = TRUE)
+    cat("Saved ", files[i], "\n")
   }
 
 }
@@ -68,7 +68,18 @@ aou_save_to_bucket <- function(file_name, bucket_name = "bucket"){
 #' @param pattern pattern like *.csv or a single file name e.g., mydata.csv
 #' @param bucket_name name of your bucket. Recommend leaving the default
 #' @export
-aou_ls <- function(pattern = "*.csv", bucket_name = "bucket"){
+aou_ls_bucket <- function(pattern = "*.csv", bucket_name = Sys.getenv('WORKSPACE_BUCKET')){
   # Check if file is in the bucket
-  system(paste0("gsutil ls ", get(bucket_name), "/data/", pattern), intern=T)
+  system(paste0("gsutil ls ", bucket_name, "/data/", pattern), intern = TRUE)
+}
+
+#' List the current files in your workspace.
+#'
+#' @param description Quick function to list files matching a pattern in your workspace
+#'
+#' @param pattern pattern like *.csv or a single file name e.g., mydata.csv
+#' @export
+aou_ls_workspace <- function(pattern = "*.csv"){
+  files <- list.files(pattern = pattern)
+  files[!grepl("*.ipynb", files)]
 }

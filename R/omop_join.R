6f30a9c53b1f2773a78d131d3123e61c63ca50dd
Louisa Smith <louisahsmith@gmail.com>
2023-06-01 18:04:25 -0400

    another way to ensure schema is provided that doesnt break in Aou?

--- R/omop_join.R
+++ R/omop_join.R
@@ -61,9 +61,11 @@ omop_join <- function(data,
   if (is.null(schema)) schema <- getOption("schema.default.value")
   if (!is.null(schema)) {
   	schema <- paste0(schema, ".")
-  } else if(grepl("redshift", con@dbms)) {
+  } else if (class(con) != "BigQueryConnection"){
+  	# if(grepl("redshift", con@dbms)) {
   	stop("Missing schema. Either provide a schema or set options(schema.default.value = cdm_schema).")
   }
+
 
   y_table = tbl(con, paste0(schema, table))
   shared_columns = intersect(colnames(data), colnames(y_table))

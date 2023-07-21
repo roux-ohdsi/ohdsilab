b6db79429a5a350f661ae360fdf7eb2f596c65cc
Louisa Smith <louisahsmith@gmail.com>
2023-07-21 12:17:44 -0400

    simplify if no wildcard, and translate to standar vocab no matter what it is

--- R/icd2omop.R
+++ R/icd2omop.R
@@ -7,28 +7,24 @@
 #' @param source_wildcard wildcard indicator in source codes. for example if code is R47.x, put "x"
 #' @param dbms_wildcard wildcard indicator for dbms LIKE function.
 #' @param translate_from source vocabulary, e.g., ICD9CM or ICD10CM. Make sure to enter the same way it is included in the concept table
-#' @param translate_to target vocabulary, e.g., SNOMED. Can be a vector. Make sure to enter the same way it is included in the concept table
 #' @param collect whether to execute the query. defaults to TRUE
 #' @param overwrite whether to overwrite the temp table created in the course of the query (#temp). defaults to TRUE
 #' @param con the connection object to the database. defaults to option "con.default.value"
 #' @param cdm_schema the schema containing the CDM. defaults to option "cdm_schema.default.value"
 
 
-#' @return a dataframe of the target and source codes, and the original codes to be matched (`orig_code`). If collect = FALSE, a reference to the SQL query.
+#' @return a dataframe of the target and source codes, and the original codes with wildcard (`orig_code`). If collect = FALSE, a reference to the SQL query.
 #' @export
 icd2omop <- function(codes,
                      source_wildcard = "x",
                      dbms_wildcard = "%",
                      translate_from = "ICD9CM",
-                     translate_to = "SNOMED",
                      collect = TRUE,
                      overwrite = TRUE,
                      con = getOption("con.default.value"),
                      cdm_schema = getOption("cdm_schema.default.value"),
                      ...){
 
-  codes_df <- data.frame(orig_code = codes,
-                         orig_code_wild = stringr::str_replace_all(codes, paste0(source_wildcard, "+"), dbms_wildcard))
 
   if (!is.null(cdm_schema)) {
     concept = paste0(cdm_schema, ".concept")
@@ -38,28 +34,34 @@ icd2omop <- function(codes,
     concept_relationship = "concept_relationship"
   }
 
-  # if(!is.null(write_schema)){
-  #   temp_tbl = paste0(write_schema, ".xzxzxzxzxzx")
-  # } else {
-  #   temp_tbl = "xzxzxzxzxzx"
-  # }
+  wild <- any(stringr::str_detect(codes, source_wildcard))
 
-  tryCatch(
-    dplyr::copy_to(dest = con, df = codes_df, name = "#temp",
-                   temporary = TRUE, overwrite = TRUE),
-    error = function(e) stop("Temporary table #temp already exists. If not needed, set `overwrite = TRUE`"))
+  if (wild) {
+    codes_df <- data.frame(orig_code = codes,
+                           orig_code_wild = stringr::str_replace_all(codes, paste0(source_wildcard, "+"), dbms_wildcard))
 
-  source_codes <- dplyr::tbl(con, concept) %>%
-    dplyr::filter(vocabulary_id == translate_from) %>%
-    dplyr::left_join(dplyr::tbl(con, "#temp"),
-                     sql_on = "concepts.concept_code LIKE my.orig_code_wild",
-                     x_as = "concepts", y_as = "my") %>%
-    dplyr::filter(!is.na(orig_code_wild)) %>%
-    dplyr::select(concept_id, source_concept_name = concept_name, source_vocabulary_id = vocabulary_id,
-                  source_code = concept_code, orig_code_wild, orig_code)
+    tryCatch(
+      dplyr::copy_to(dest = con, df = codes_df, name = "#temp",
+                     temporary = TRUE, overwrite = TRUE),
+      error = function(e) stop("Temporary table #temp already exists. If not needed, set `overwrite = TRUE`"))
+
+    source_codes <- dplyr::tbl(con, concept) %>%
+      dplyr::filter(vocabulary_id == translate_from) %>%
+      dplyr::left_join(dplyr::tbl(con, "#temp"),
+                       sql_on = "concepts.concept_code LIKE my.orig_code_wild",
+                       x_as = "concepts", y_as = "my") %>%
+      dplyr::filter(!is.na(orig_code_wild)) %>%
+      dplyr::select(concept_id, source_concept_name = concept_name, source_vocabulary_id = vocabulary_id,
+                    source_code = concept_code, orig_code_wild, orig_code)
+  } else {
+    source_codes <- dplyr::tbl(con, concept) %>%
+      dplyr::filter(vocabulary_id == translate_from, concept_code %in% codes) %>%
+      dplyr::select(concept_id, source_concept_name = concept_name, source_vocabulary_id = vocabulary_id,
+                    source_code = concept_code)
+  }
 
   target_codes <- dplyr::tbl(con, concept) %>%
-    dplyr::filter(vocabulary_id %in% translate_to) %>%
+    dplyr::filter(standard_concept = "S") %>%
     dplyr::select(concept_id, target_concept_name = concept_name, target_vocabulary_id = vocabulary_id)
 
   relationships <- dplyr::tbl(con, concept_relationship) %>%
@@ -68,7 +70,7 @@ icd2omop <- function(codes,
     dplyr::inner_join(target_codes, by = c("concept_id_2" = "concept_id"), y_as = "target") %>%
     dplyr::rename(concept_id = concept_id_2,
                   orig_concept_id = concept_id_1) %>%
-    dplyr::select(-orig_code_wild) %>%
+    dplyr::select(-any_of("orig_code_wild")) %>%
     dplyr::distinct()
 
   if (collect) dbi_collect(relationships)
